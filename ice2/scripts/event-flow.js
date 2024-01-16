169162285df8dc3ed7bff3cac5b1229bb80629cd
Talent Nyota <talent.nyota@dcmail.ca>
2024-01-16 13:38:54 -0500

    ice2 updated

--- ice2/scripts/event-flow.js
+++ ice2/scripts/event-flow.js
@@ -1,43 +1,43 @@
 //#region BUBBLING AND CAPTURING FUNCTIONS
 // function to show the innerHTML of the element bound with the click event to this function
 function showElement(e) {  
-  // e.cancelBubble = true;                           // prevent the bubbling up through the elements with a click on the innermost element
-  // e.stopPropagation();                             // prevents bubbling and capturing
-  // alert("Using 'e.target': " + e.target.innerHTML);  // event is the element that recieved the click
+  e.cancelBubble = true;                           // prevent the bubbling up through the elements with a click on the innermost element
+  e.stopPropagation();                             // prevents bubbling and capturing
+  alert("Using 'e.target': " + e.target.innerHTML);  // event is the element that recieved the click
   alert("Using 'this': " + this.innerHTML);          // this is the element that fired the event  
-  // alert("This is only only link element node...or is it?")
+  alert("This is only only link element node...or is it?")
 }
 
-// function writeSomething(e) {
-//   alert("This is on the parent item element node.")
-// }
+function writeSomething(e) {
+  alert("This is on the parent item element node.")
+}
 
-// function grandparent(e) {
-//   alert("This is on the grandparent list element node.")
-// }
-//#endregion
+function grandparent(e) {
+  alert("This is on the grandparent list element node.")
+}
+// #endregion
 
 //#region EVENT BUBBLING 
 
 // because clicking on a child within a parent necessarily clicks on the parent, if both have a click event wired up, that click event fires even if the events are different
-// let link_bubble = document.getElementById("link");
-// link.addEventListener('click', showElement, false);
+let link_bubble = document.getElementById("link");
+link.addEventListener('click', showElement, false);
 
 // event bubbling when using the different events
-// let item_bubble = document.getElementById('item');
-// item.addEventListener('click', showElement, false);
+let item_bubble = document.getElementById('item');
+item.addEventListener('click', showElement, false);
 
 // event bubbling when using different events
-// let list_bubble = document.getElementById('list');
-// list.addEventListener('click', showElement, false)
+let list_bubble = document.getElementById('list');
+list.addEventListener('click', showElement, false)
 
 // event bubbling when using the different events
-// let item_bubble2 = document.getElementById('item');
-// item.addEventListener('click', writeSomething, false);
+let item_bubble2 = document.getElementById('item');
+item.addEventListener('click', writeSomething, false);
 
 // event bubbling when using different events
-// let list_bubble2 = document.getElementById('list');
-// list.addEventListener('click', grandparent, false)
+let list_bubble2 = document.getElementById('list');
+list.addEventListener('click', grandparent, false)
 
 //#endregion
 
