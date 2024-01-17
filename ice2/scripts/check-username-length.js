69c83800f12c192a680594445b094c9505564f96
Talent Nyota <talent.nyota@dcmail.ca>
2024-01-17 17:12:10 -0500

    Updated check-username-length.js

--- ice2/scripts/check-username-length.js
+++ ice2/scripts/check-username-length.js
@@ -1,3 +1,13 @@
+/**
+ * File: check-username-length.js
+ * Author: Talent Nyota
+ * Student Number: 100761242
+ * Date: 2024-01-17
+ * Description: This script is used for validating the length of the username in a form. 
+ *              It checks if the entered username meets the minimum length requirement.
+ *              This script is part of the ICE2 project for the WEBD6201 course.
+ */
+
 function checkLength(e, minLength) {            // Declare function
   console.log("Debug at start of checkLength function") // Debug statement
   let el, elMsg;                                // Declare variables  
@@ -11,10 +21,14 @@ function checkLength(e, minLength) {            // Declare function
   }
 }
 
-let elUsername = document.getElementById('username');// Get username input
-if (elUsername.addEventListener) {           // If event listener supported
-  elUsername.addEventListener('blur', function(e) {  // On blur event
-    // NOTE: This function is checkLength() - not checkUsername()
-    checkLength(e, 5);                             // Call checkLength()
-  }, false);                                       // Capture in bubble phase
-} 
+let elUsername = document.getElementById('username'); // Get username input element
+
+// Check if the browser supports event listeners
+if (elUsername.addEventListener) {           
+  // Add a blur event listener to the username input
+  elUsername.addEventListener('blur', function(e) {  
+    // Call checkLength function when blur event occurs
+    // It validates if the username length is at least 5 characters
+    checkLength(e, 5);                             
+  }, false); // False indicates event capturing is not used
+}
